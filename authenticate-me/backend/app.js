84aac07a343dacf38f48cad7730434807c147c08
anthony <awadams198@gmail.com>
2022-01-07 20:19:42 -0500

    api login finished

--- authenticate-me/backend/app.js
+++ authenticate-me/backend/app.js
@@ -5,6 +5,7 @@ const csurf = require("csurf");
 const helmet = require("helmet");
 const cookieParser = require("cookie-parser");
 const routes = require("./routes");
+const { ValidationError } = require("sequelize");
 
 const { environment } = require("./config");
 const isProduction = environment === "production";
@@ -48,6 +49,16 @@ app.use(
   })
 );
 
-
+// Error formatter
+app.use((err, _req, res, _next) => {
+  res.status(err.status || 500);
+  console.error(err);
+  res.json({
+    title: err.title || "Server Error",
+    message: err.message,
+    errors: err.errors,
+    stack: isProduction ? null : err.stack,
+  });
+});
 
 module.exports = app;
