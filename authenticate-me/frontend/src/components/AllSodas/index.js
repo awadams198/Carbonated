5ce8b3e90462ef4a4d6ff0ddd61e3b77641aeb8c
anthony <awadams198@gmail.com>
2022-01-13 15:04:28 -0500

    fixed test

--- authenticate-me/frontend/src/components/AllSodas/index.js
+++ authenticate-me/frontend/src/components/AllSodas/index.js
@@ -23,7 +23,6 @@ const AllSodas = ({ isLoaded }) => {
                 <div className="soda-card" key={`${soda?.id}`}>
                   <h4 className="soda-card-h">{soda?.name}</h4>
                   <p className="soda-card-b">{soda?.body}</p>
-                  <p>TESTING</p>
                 </div>
               </NavLink>
             ))}
